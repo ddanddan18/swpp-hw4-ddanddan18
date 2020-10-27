e69c5de18837f65d42573bb0e6d954710acb42c6
ddanddan18 <bob070030@gmail.com>
2020-10-28 02:32:22 +0900

    [add] admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Comment, Article
 
 # Register your models here.
+admin.site.register(Article)
+admin.site.register(Comment)
